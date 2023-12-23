7ce0595f02e0b9eeda5ac5299a09e42408635a83
Dmitry Matul <matuldmtr@gmail.com>
2023-12-23 09:49:56 +0100

    feat: dns-stats and encode-line complete

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const res = {};
+
+  domains.forEach((domain) => {
+    const subDomains = domain.split(".").reverse();
+    let current = "";
+
+    subDomains.forEach((subDomain) => {
+      current += `.${subDomain}`;
+      res[current] = res[current] ? res[current] + 1 : 1;
+    });
+  });
+  return res;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const res = [];
+  let counter = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1]) {
+      if (counter !== 1) {
+        res.push(counter);
+      }
+      res.push(str[i]);
+      counter = 1;
+    } else {
+      counter++;
+    }
+  }
+  return res.join("");
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
